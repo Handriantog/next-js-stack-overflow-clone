c9f5dab419a3bb9079900cb53ab2071781fb5490
Han <handriantog@gmail.com>
2025-02-16 00:28:59 +0800

    Add fonts and assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
 const Home = () => (
-  <h1 className="text-3xl font-bold text-white">Hello world!</h1>
+  <>
+    <h1 className="h1-bold text-3xl font-bold text-black">
+      Hello From Next Js!
+    </h1>
+    <h1 className="h1-bold font-space-grotesk text-3xl font-bold text-black">
+      Using Inter font
+    </h1>
+  </>
 );
 
 export default Home;
